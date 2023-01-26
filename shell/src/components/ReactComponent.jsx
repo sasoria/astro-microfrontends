b957c707e475e2ef87e4ff4515ccfce26f108009
sasoria <sergio.alberto.arevalo.soria@nav.no>
2023-01-26 13:36:25 +0100

    Clean up

--- shell/src/components/ReactComponent.jsx
+++ shell/src/components/ReactComponent.jsx
@@ -2,11 +2,11 @@ import React from "https://esm.sh/react"
 import MicroFrontendA from "http://localhost:7100/bundle.js"
 import MicroFrontendB from "http://localhost:7200/bundle.js"
 
-const MyReactComponent = () => (
+const ReactComponent = () => (
   <>
     <MicroFrontendA />
     <MicroFrontendB />
   </>
 );
 
-export default MyReactComponent;
+export default ReactComponent;
