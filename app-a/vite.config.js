9f6805daab7699efa75f9b905a58ce02a2fb13a4
sasoria <sergio.alberto.arevalo.soria@nav.no>
2023-02-08 09:57:42 +0100

    Shared dependencies

--- app-a/vite.config.js
+++ app-a/vite.config.js
@@ -1,10 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import cssInject from "vite-plugin-css-injected-by-js";
+import { rollupImportMapPlugin } from "rollup-plugin-import-map";
 import { resolve } from "path";
+import importmap from "./importmap.json";
 
 export default ({ command }) => ({
-  plugins: [react(), cssInject()],
+  plugins: [
+    react(),
+    cssInject(),
+    {
+      ...rollupImportMapPlugin(importmap),
+      enforce: "pre",
+      apply: "build",
+    },
+  ],
   build: {
     rollupOptions: {
       input: resolve(__dirname, "src/MicroFrontend.jsx"),
