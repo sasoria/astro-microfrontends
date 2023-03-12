b99a8f7d99b2227156c5911f945b416d8a8ae765
sasoria <sergio.alberto.arevalo.soria@nav.no>
2023-03-12 08:34:49 +0100

    Use importmap

--- shell/astro.config.mjs
+++ shell/astro.config.mjs
@@ -1,9 +1,27 @@
 import { defineConfig } from 'astro/config';
+import importmap from "./importmap.json";
+import { rollupImportMapPlugin } from "rollup-plugin-import-map";
 
 // https://astro.build/config
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [
+    react(),
+    {
+      name: 'importmap',
+      hooks: {
+        'astro:build:setup': ({ vite, target }) => {
+          if(target === 'client') {
+            vite.plugins.push({
+              ...rollupImportMapPlugin(importmap),
+              enforce: "pre",
+              apply: "build",
+            });
+          }
+       }
+      }
+    }
+  ]
 });
