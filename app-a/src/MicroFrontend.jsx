eec3e0b725d9a17279b9371336ef24af278c5d91
sasoria <sergio.alberto.arevalo.soria@nav.no>
2023-03-12 10:55:58 +0100

    Remove inject functions

--- app-a/src/MicroFrontend.jsx
+++ app-a/src/MicroFrontend.jsx
@@ -2,12 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
-export const injectMicroFrontendA = (parentElementId) => {
-  const container = document.getElementById(parentElementId);
-  const root = createRoot(container);
-  root.render(<App />);
-};
-
 const MicroFrontend = () => <App />;
 
 export default MicroFrontend;
--- shell/src/components/ReactComponent.jsx
+++ shell/src/components/ReactComponent.jsx
@@ -1,23 +1,12 @@
 import React, { useEffect } from "react";
-import { injectMicroFrontendA } from "http://localhost:7100/bundle.js";
-import { injectMicroFrontendB } from "http://localhost:7200/bundle.js";
+import MicorFrontendA from "http://localhost:7100/bundle.js";
+import MicorFrontendB from "http://localhost:7200/bundle.js";
 
-const mountpointA = "microfrontend-a";
-const mountpointB = "microfrontend-b";
-
-const ReactComponent = () => {
-
-  useEffect(() => {
-    injectMicroFrontendA(mountpointA);
-    injectMicroFrontendB(mountpointB);
-  }, []);
-
-  return (
-    <>
-      <div id={mountpointA} />
-      <div id={mountpointB} />
-    </>
-  );
-};
+const ReactComponent = () => (
+  <>
+    <MicorFrontendA />
+    <MicorFrontendB />
+  </>
+);
 
 export default ReactComponent;
