2bc040241ec180a3a3b4feb3acf1ca26904301ef
sasoria <sergio.alberto.arevalo.soria@nav.no>
2023-03-12 11:42:33 +0100

    Fix typo

--- shell/src/components/ReactComponent.jsx
+++ shell/src/components/ReactComponent.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
-import MicorFrontendA from "http://localhost:7100/bundle.js";
-import MicorFrontendB from "http://localhost:7200/bundle.js";
+import MicroFrontendA from "http://localhost:7100/bundle.js";
+import MicroFrontendB from "http://localhost:7200/bundle.js";
 
 const ReactComponent = () => (
   <>
-    <MicorFrontendA />
-    <MicorFrontendB />
+    <MicroFrontendA />
+    <MicroFrontendB />
   </>
 );
 
