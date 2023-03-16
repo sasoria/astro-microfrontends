5cb06a19155873011cd8248fe47dc78592569ae5
sasoria <sergio.alberto.arevalo.soria@nav.no>
2023-03-16 11:57:45 +0100

    Re-introduce solid app

--- shell/src/components/SolidComponent.jsx
+++ shell/src/components/SolidComponent.jsx
@@ -2,10 +2,9 @@ import MicroFrontendC from "http://localhost:7300/bundle.js";
 
 function App() {
   return (
-    <section>
-        <h1>Solid App</h1>
-        <MicroFrontendC />
-    </section>
+    <>
+      <MicroFrontendC />
+    </>
   );
 }
 
