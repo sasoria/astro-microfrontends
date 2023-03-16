8ae7e58ab4979605a1488386a623e048e6b16bcb
sasoria <sergio.alberto.arevalo.soria@nav.no>
2023-03-16 19:23:55 +0100

    Remove importmap plugin

--- shell/astro.config.mjs
+++ shell/astro.config.mjs
@@ -1,31 +1,20 @@
 import { defineConfig } from "astro/config";
-import importmap from "./importmap.json";
-import { rollupImportMapPlugin } from "rollup-plugin-import-map";
-
-// https://astro.build/config
 import react from "@astrojs/react";
-
-// https://astro.build/config
 import solidJs from "@astrojs/solid-js";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [
     react(),
     solidJs(),
     {
-      name: "importmap",
+      name: 'importmap-externals',
       hooks: {
-        "astro:build:setup": ({ vite, target }) => {
-          if (target === "client") {
-            vite.plugins.push({
-              ...rollupImportMapPlugin(importmap),
-              enforce: "pre",
-              apply: "build",
-            });
+        'astro:build:setup': ({ vite, target }) => {
+          if(target === 'client') {
+            vite.build.rollupOptions["external"] = ["react", "react-dom"];
           }
-        },
-      },
+        }
+       }
     },
   ],
 });
